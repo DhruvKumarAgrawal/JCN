0bb486ebcdfa21cc1c49d7cae57725b01c1489c5
Dhruv Kumar (Intern - NPD/E-City/TVSMotor) <Dhruv.Kumar@tvsmotor.com>
2024-06-11 17:09:56 +0530

    added background and  header

--- jcn-app/src/app/app.module.ts
+++ jcn-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
